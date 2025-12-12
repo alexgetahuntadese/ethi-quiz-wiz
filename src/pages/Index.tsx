3e75a514cf18063bc191e904e2309a774024b0ec
alexgetahuntadese <alexgetahuntadese@gmail.com>
2025-12-12 11:41:35 +0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -55,6 +55,7 @@ const Index = () => {
               <div className="text-sm">Grade Levels</div>
             </div>
             <div>
+              <p>HIIIIIIIIIIIIIIIIIIII<p/>
               <div className="text-2xl font-bold text-pink-400">AIIIIIIIIIIIIIII</div>
               <div className="text-sm">Powered Solution For Students</div>
             </div>
